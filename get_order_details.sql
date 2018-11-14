862680e9c71e317bb449a224ecd702f8c9aef774
yvonne <yvonne250003@gmail.com>
2018-11-14 11:15:53 +0800

    1114 sql訂單明細

---
+++ get_order_details.sql
@@ -0,0 +1,13 @@
+SELECT
+  o.*,
+  d.`product_sid`,
+  d.`price`,
+  d.`quantity`,
+  p.`bookname`,
+  p.`author`
+FROM `orders` o
+  JOIN `order_details` d
+    ON o.sid=d.order_sid
+  JOIN products p
+    ON p.sid=d.product_sid
+  WHERE o.member_sid=9;
